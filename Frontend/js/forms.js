24e3e9efe2cbd9eb32a77bb6e44412cf4b3a8b80
joaoVelosoT <vitorvteixeira2023@gmail.com>
2024-10-21 14:03:38 -0300

    adicionando a senha na lista

--- Frontend/js/forms.js
+++ Frontend/js/forms.js
@@ -3,9 +3,45 @@ function togglePopup() {
     overlay.classList.toggle('show');
 }
 
-function clicked(e)
-{
-    if(!confirm('Are you sure?')) {
-        e.preventDefault();
-    }
-}
+// function clicked(e)
+// {
+//     if(!confirm('Are you sure?')) {
+//         e.preventDefault();
+//     }
+// } 
+
+
+
+
+// Adicionando senha na lista
+
+const name = document.getElementById("name");
+const form = document.getElementById("form-container");
+
+form.addEventListener('submit', async(e) => {
+    e.preventDefault();
+
+
+
+    console.log("fez o submit");
+    console.log(name.value);
+
+    const data = JSON.stringify({
+        nome : name.value,
+        status : 'Pendente'
+    })
+
+    const response = await fetch('http://localhost:5000/fila', {
+        method : 'POST',
+        headers : {"Content-type": "application/json; charset=UTF-8"},
+        body : data
+    })
+
+    const json = await response.json();
+
+    console.log(json);
+
+
+    window.location.reload();
+    alert('Cadastro feito com sucesso')
+})
