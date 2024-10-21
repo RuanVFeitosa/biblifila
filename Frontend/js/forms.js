57fc5f5b76b7f07c2f175309ba2b98a57cde5c88
Ruan Viana Feitosa <vianafeitosaruan@gmail.com>
2024-10-21 13:45:05 -0300

    Finalização do Front

--- Frontend/js/forms.js
+++ Frontend/js/forms.js
@@ -2,3 +2,10 @@ function togglePopup() {
     const overlay = document.getElementById('popupOverlay');
     overlay.classList.toggle('show');
 }
+
+function clicked(e)
+{
+    if(!confirm('Are you sure?')) {
+        e.preventDefault();
+    }
+}
