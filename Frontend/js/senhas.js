3d2e82893ace84a942828f89ef40862740015839
Ruan Viana Feitosa <vianafeitosaruan@gmail.com>
2024-10-23 09:52:48 -0300

    Arrumando as Senhas.js

--- Frontend/js/senhas.js
+++ Frontend/js/senhas.js
@@ -16,6 +16,28 @@ const limitarSenhas = (container, tipo) => {
 limitarSenhas(containerProx, 'prox');
 limitarSenhas(containerCham, 'cham');
 
+const proximosLista = async() => {
+    const response = await fetch('http://localhost:5000/fila/pendentes', {
+        method : "GET",
+        headers : {},
+    })
+
+    const json = await response.json();
+    const pendentes = json.pendentes;
+    pendentes.forEach(element => {
+
+        containerProx.innerHTML += 
+        <div class="prox">${element.senha}</div>
+        // console.log(element)
+    });
+    // console.log(pendentes);
+}
+
+// setTimeout(function (){
+//     window.location.reload(1);
+// }, 5000)
+
+// window.location.reload();
 
 const chamadosLista = async () => {
     try {
